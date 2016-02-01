00e7807ba304eba2f308668dfd2af239b93e57bb
TwoRivers <krquddnr37@naver.com>
2016-02-02 06:46:11 +0900

    writev,readv Áּ® Ã߰¡

--- C/C_Socket/Linux/readv.c
+++ C/C_Socket/Linux/readv.c
@@ -13,7 +13,11 @@ int main(int argc,char**argv){
 	vec[0].iov_len=5;
 	vec[1].iov_base=buf2;
 	vec[1].iov_len=BUF_SIZE;
-
+	
+	//파일디스크립터,iovec 구조체의 배열,배열의 갯수
+	//iovec 구조체 ->iov_base,iov_len 으로 구성
+	//iov_base는 버퍼주소(void* 타입)
+	//iov_len 은 버ㅓ퍼의 크기
 	str_len=readv(0,vec,2);
 	printf("Read bytes : %d\n",str_len);
 	printf("First message : %s\n",buf1);
--- C/C_Socket/Linux/writev.c
+++ C/C_Socket/Linux/writev.c
@@ -11,6 +11,9 @@ int main(int argc, char**argv){
 	vec[1].iov_base=buf2;
 	vec[1].iov_len=5;
 	//파일디스크립터,iovec 구조체의 배열,배열의 갯수
+	//iovec 구조체 ->iov_base,iov_len 으로 구성
+	//iov_base는 버퍼주소(void* 타입)
+	//iov_len 은 버ㅓ퍼의 크기
 	str_len=writev(1,vec,2);
 	puts("");
 	printf("Write bytes : %d \n",str_len);
